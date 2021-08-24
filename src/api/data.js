99ce3cf1d63ba4ca99d5dc21f50edb6e92fbd815
lboyadzhiev <l.boyadzhievv@abv.bg>
2021-08-24 14:20:01 +0300

    register function

--- src/api/data.js
+++ src/api/data.js
@@ -12,3 +12,13 @@ export async function login(email, password) {
 
     return result;
 }
+
+export async function register(email, password, repeatPassword) {
+    const result = await api.post(host + '/users/register', { email, password, repeatPassword });
+
+    sessionStorage.setItem('emial', result.email);
+    sessionStorage.setItem('authToken', result.accessToken);
+    sessionStorage.setItem('userId', result._id);
+
+    return result;
+}
